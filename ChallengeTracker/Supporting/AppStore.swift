857e0d1505a78c5e41822d3d68c98d21c8acf41c
Nigel Gee <41487836+NigelGee@users.noreply.github.com>
2022-06-04 12:25:21 +0100

    Change to AppStore for review not working

--- ChallengeTracker/Supporting/AppStore.swift
+++ ChallengeTracker/Supporting/AppStore.swift
@@ -11,29 +11,19 @@ import StoreKit
 class AppStore: ObservableObject {
     var threshold = 10
     @AppStorage("runsSinceLastRequest") var runsSinceLastRequest = 0
-    @AppStorage("version") var version = ""
 
     @Published var showingOverlay = false
     let configuration = SKOverlay.AppConfiguration(appIdentifier: "1465159349", position: .bottom)
 
     func check() {
         runsSinceLastRequest += 1
-        let appBuild = Bundle.main.object(forInfoDictionaryKey: "CFBundleVersion") as! String
-        let appVersion = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as! String
-        let thisVersion = "v\(appVersion)b\(appBuild)"
 
-        if thisVersion != version {
-            if runsSinceLastRequest >= threshold {
-                if let scene = UIApplication.shared.connectedScenes.first as? UIWindowScene {
-                    SKStoreReviewController.requestReview(in: scene)
-                    version = thisVersion
-                    runsSinceLastRequest = 0
-                }
-            } else if runsSinceLastRequest == threshold - 2 {
-                showingOverlay = true
+        if runsSinceLastRequest >= threshold {
+            if let scene = UIApplication.shared.connectedScenes.first as? UIWindowScene {
+                SKStoreReviewController.requestReview(in: scene)
             }
-        } else {
-            runsSinceLastRequest = 0
+        } else if runsSinceLastRequest >= threshold - 3 && runsSinceLastRequest <= threshold {
+            showingOverlay = true
         }
     }
 }
